f3df962d49561a42d0957c2ca7d9d795a08e8f5e
jhuang2github <susanhuang77@gmail.com>
2014-10-19 22:53:08 -0700

    some notes

--- ImageSearch/AppDelegate.mm
+++ ImageSearch/AppDelegate.mm
@@ -15,11 +15,17 @@
 //}
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
-    // Programmatically create window
+    // Programmatically create window and view
+    //https://developer.apple.com/library/ios/documentation/WindowsViews/Conceptual/ViewPG_iPhoneOS/CreatingViews/CreatingViews.html#//apple_ref/doc/uid/TP40009503-CH5-SW1
+
 //    self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
 //    self.window.backgroundColor = [UIColor whiteColor];
 //    [self.window makeKeyAndVisible];
-
+//
+//    CGRect  viewRect = CGRectMake(0, 0, 100, 100);
+//    UIView* myView = [[UIView alloc] initWithFrame:viewRect];
+//
+//    [self.window addSubview:myView];
     return YES;
 }
 
